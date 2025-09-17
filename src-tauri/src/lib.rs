86f251cfd3349178ce54cbaf4122f4ed9b591fef
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-09-17 16:51:40 -0400

    Initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,34 @@
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] 
+			{
+				let log_plugin = tauri_plugin_log::Builder::default()
+					.level(log::LevelFilter::Info)
+					.build();
+
+				app.handle().plugin(log_plugin)?;
+			}
+			
+			#[cfg(debug_assertions)]
+            {
+                use tauri::Manager;
+
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+
+export { default as react_logo} from "./logos/react.svg";
+export { default as tauri_logo} from "./logos/tauri.svg";
+export { default as vite_logo} from "./logos/vite.svg"
+export { default as ascribe_logo} from "./logos/ascribe.svg"
