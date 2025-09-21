ce486a2dca7b1b2fff159bad18e93c81d95e6f02
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-09-21 15:56:44 -0400

    wip

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,1 @@
+use tauri::{Runtime, ipc::Invoke};
--- src-tauri/src/core/settings.rs
+++ src-tauri/src/core/settings.rs
@@ -1,4 +1,16 @@
+use std::sync::{Arc, Mutex};
+
 use serde::{Deserialize, Serialize};
+use tauri::{Emitter, State};
+
+pub const SETTINGS_CHANGED_EVENT_NAME: &str = "settings-changed";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SettingsChangedEvent 
+{
+    pub old: AppSettings,
+    pub new: AppSettings,
+}
 
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -11,8 +23,38 @@ impl Default for AppSettings
 {
     fn default() -> Self 
     {
-        Self { 
+        Self 
+        { 
             ui_scale: 1.0
         }
+    }
+}
+
+#[tauri::command(rename_all = "snake_case")]
+pub fn run_settings_command(
+    state: State<'_, Mutex<AppSettings>>, 
+    app_handle: tauri::AppHandle,
+
+    command: &str, 
+    value: Option<AppSettings>
+) -> Option<AppSettings>
+{
+    match command 
+    {
+        "get" => {
+            Some(state.lock().unwrap().clone())
+        },
+        "set" => {
+            let mut settings = state.lock().unwrap();
+            let old = settings.clone();
+            *settings = value.unwrap();
+
+            app_handle.emit(SETTINGS_CHANGED_EVENT_NAME, SettingsChangedEvent {
+                old: old,
+                new: settings.clone(),
+            }).unwrap();
+            None
+        },
+        _ => panic!("Unknown settings sub command {}", command)
     }
 }
