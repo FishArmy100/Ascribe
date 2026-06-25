74080ed64d3fe48cef9f7653d076e1834019a47a
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2026-06-24 23:19:03 -0400

    added a basic save system

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,5 @@
-use std::sync::Mutex;
-use tauri::Manager;
+use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
+use tauri::{Manager, WindowEvent};
 use crate::{bible::{BibleDisplaySettings, BiblioJsonPackageHandle, printing::printing_state::PrintBibleState}, core::{app::AppState, settings::{self, AppSettings}, view_history::{self, ViewHistory}}, reader::BibleReaderBehavior, sfx::SfxPlayer, tts::{TtsAudioLibrary, gen_thread::TtsGenThread, init_espeak, player::TtsPlayer, voices::AppVoices}};
 
 pub mod core;
@@ -12,7 +12,10 @@ pub mod sfx;
 pub mod reader;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub fn run() 
+{
+    let is_closing = Arc::new(AtomicBool::new(false));
+    
     tauri::Builder::default()
         .setup(|app| {
             
@@ -42,16 +45,32 @@ pub fn run() {
             app.manage(TtsAudioLibrary::new(app.handle().clone()));
             app.manage(TtsPlayer::new(app.handle().clone()));
 
-            app.manage(Mutex::new(AppState {
-                settings: AppSettings::default(),
-                bible_display_settings: BibleDisplaySettings::default(),
-                view_history: ViewHistory::new(),
-                reader_behavior: BibleReaderBehavior::default(),
-            }));
+            app.manage(Mutex::new(AppState::load(app.path()).unwrap()));
 
             BibleDisplaySettings::add_on_package_init_listener(app.handle().clone());
 
             Ok(())
+        })
+        .on_window_event(move |window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event 
+            {
+                // If we're already in the process of closing, allow it
+                if is_closing.load(Ordering::SeqCst) 
+                {
+                    return;
+                }
+
+                api.prevent_close();
+                is_closing.store(true, Ordering::SeqCst);
+
+                let app = window.app_handle();
+                let state = app.state::<Mutex<AppState>>();
+                let binding = state.lock().unwrap();
+                binding.save(app.path()).unwrap();
+                drop(binding);
+
+                window.close().unwrap();
+            }
         })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
